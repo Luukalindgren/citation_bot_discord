8c03868688239c39107cf7939f1056d58e71a954
luukalindgren <luslin@utu.fi>
2023-12-05 11:39:05 +0200

    Bot sends quote every 24h

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import discord
 import os
+from discord.ext import tasks
 from dotenv import load_dotenv
 from fetch_data import get_random_summary
 
@@ -14,9 +15,26 @@ client = discord.Client(intents=intents)
 token = os.getenv('BOT_TOKEN')
 
 
+def format_quote(summary):
+    formatted_message = (
+
+        f"## *{summary['random_quote'].strip()}*\n"
+        "```\n"
+        f"Kirja: {summary['title']}\n"
+        f"Kirjailija: {summary['author']}\n"
+        f"Kirjan arvosana: ({summary['rating']}/5)\n"
+        "```\n"
+        "**[Kirjan tiivistelmä](https://blog-nextjs-three-nu.vercel.app/books)**"
+
+    )
+
+    return formatted_message
+
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    daily_quote.start()
 
 
 @client.event
@@ -27,17 +45,19 @@ async def on_message(message):
     if message.content.startswith('!Quote'):
         summary = get_random_summary()
 
-        formatted_message = (
-
-            f"## *{summary['random_quote'].strip()}*\n"
-            "```\n"
-            f"Kirja: {summary['title']}\n"
-            f"Kirjailija: {summary['author']}\n"
-            f"Kirjan arvosana: ({summary['rating']}/5)\n"
-            "```\n"
-            "**[Kirjan tiivistelmä](https://blog-nextjs-three-nu.vercel.app/books)**"
-        )
+        formatted_message = format_quote(summary)
 
         await message.channel.send(formatted_message)
+
+
+@tasks.loop(hours=24)
+async def daily_quote():
+    await client.wait_until_ready()
+    channel = client.get_channel(1181191090817216565)
+
+    summary = get_random_summary()
+    formatted_message = format_quote(summary)
+    await channel.send(formatted_message)
+
 
 client.run(token)
